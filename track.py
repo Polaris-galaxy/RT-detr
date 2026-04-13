87997755e85900d05e7d56ad51a2cf579da7867e
Polaris-galaxy <3144757020@qq.com>
2026-04-13 17:44:24 +0800

    环境以及稳定

--- track.py
+++ track.py
@@ -4,9 +4,10 @@ from ultralytics import RTDETR
 
 if __name__ == '__main__':
     # 推理/跟踪优先使用验证集最优权重 best.pt；last.pt 为最后一轮，末期若过拟合可能较差
-    model = RTDETR(r'runs\train\wool_small_obj\weights\best.pt')
+    model = RTDETR(r'runs\train\exp2\weights\best.pt')
     model.track(source=r'D:\Galaxy\其他\桌面\output_video53.avi',
                 project='runs/track',
                 name='exp',
-                save=True
+                save=True,
+                stream=True,
                 )
